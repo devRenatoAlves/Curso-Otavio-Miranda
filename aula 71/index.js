e0245e687b6b3caadc70adf3f934d791f708cc52
Renato Alves <devrenatoalves@gmail.com>
2024-09-25 21:39:44 -0300

    Day 1

--- aula 71/index.js
+++ aula 71/index.js
@@ -5,12 +5,18 @@ function Produto (nome, preco, estoque) {
     
     Object.defineProperty(this, 'estoque', {
         enumerable: true, // Mostra a chave?
-        value: estoque, // definir o Valor da chave
-        writable: false, // Pode alterar o valor?
-        configurable: true // Pode configurar a chave?
+        configurable: true, // Pode configurar a chave?
+        get: function () {
+            return estoque;
+        },
+        set: function (valor) {
+        console.log(valor)
+        }
     })
 };
   
 const p1 = new Produto ('Feijao', 10, 4)
-
 console.log (p1)
+
+p1.estoque = 'O valor  que eu quero'
+console.log(p1.estoque)
